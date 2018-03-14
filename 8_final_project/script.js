bb4a7afc578e107b28ad1e8711c518ef4ce422b8
Will <willsch2704@gmail.com>
2018-03-14 12:36:32 +0000

    commenting practice

--- 8_final_project/script.js
+++ 8_final_project/script.js
@@ -1,5 +1,10 @@
 /* global $ */
+var comment;
 function math(naut){
     var meters = naut * 1852;
     var smoot = meters / 1.7;
 }
+$( "#commentBttn" ).click(function() {
+    comment = $("#commentInput").val();
+    $("#commentsP").append(comment);
+});
